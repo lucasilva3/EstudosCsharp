89dae85d7923667af0aee047834730aee3f4268c
Lucas <a>
2024-06-25 18:08:24 -0300

    Adicionando Estudo ParOuImpar

---
+++ ParOuImpar/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParOuImpar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira um valor numérico: ");
+            var valor_numerico = Convert.ToInt16(Console.ReadLine());
+
+            String par_inp = (valor_numerico % 2 == 0) ? "par" : "ímpar";
+            String pos_neg = (valor_numerico >= 0) ? "positivo" : "negativo";
+
+            Console.WriteLine("\n[O valor inserido é "+ par_inp+ " e "+ pos_neg+ "]");
+        }
+    }
+}
+
